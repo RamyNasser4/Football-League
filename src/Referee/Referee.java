3b296ae36cbf1b33940f77c98deafe269c4e9be9
ghazaly118 <amohahazaly@gmail.com>
2023-11-23 16:17:28 +0200

    static removed display method added

--- src/Referee/Referee.java
+++ src/Referee/Referee.java
@@ -5,20 +5,22 @@ public class Referee {
     private int RefereeAge;
 //   public Match match =new Match();
     //
-    public static int  noYellowCards=0;
-    public static int noRedCards=0;
+    public  int  noYellowCards=0;
+    public  int noRedCards=0;
 
     public Referee(String refereeName, int refereeAge/*, Match match*/) {
         RefereeName = refereeName;
         RefereeAge = refereeAge;
 //        this.match = match;
+        noYellowCards=0;
+        noRedCards=0;
     }
 
-    public static void makeYellowCards(){
+    public  void makeYellowCards(){
 
         noYellowCards++;
     }
-    public static void makeRedCards(){
+    public  void makeRedCards(){
 
         noRedCards++;
     }
@@ -37,5 +39,22 @@ public class Referee {
 
     public void setRefreeAge(int refreeAge) {
         this.RefereeAge = refreeAge;
+    }
+    public void display (){
+        System.out.println("the Referee name:  ");
+        System.out.println(RefereeName);
+        System.out.println("the Referee age:  ");
+        System.out.println(RefereeAge);
+        System.out.println("the number of yellow cards in this match:  ");
+        System.out.println(noYellowCards);
+        System.out.println("the number of red cards in this match:  ");
+        System.out.println(noRedCards);
+
+
+
+
+
+
+
     }
 }

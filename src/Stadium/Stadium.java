641905204f820a5011a43befec0bd30f0a54f480
sebaey124 <abdelrahmansebaay0@gmail.com>
2023-11-24 23:10:31 +0200

    commenting fn in stadium

--- src/Stadium/Stadium.java
+++ src/Stadium/Stadium.java
@@ -7,46 +7,49 @@ public class Stadium {
     public int capacity;
     public String Date;
 
-    public Stadium(String name,String location,int capacity,String date){
-        this.name=name;
-        this.location=location;
-        this.capacity=capacity;
-        this. Date=date;
+    public Stadium(String name, String location, int capacity, String date) {
+        this.name = name;
+        this.location = location;
+        this.capacity = capacity;
+        this.Date = date;
 
     }
 
     public String getStadiumName() {
-        return name ;
+        return name;
     }
 
     public void setStadiumName(String StadiumName) {
         this.name = StadiumName;
     }
+
     public String getStadiumLocation() {
-        return location ;
+        return location;
     }
 
     public void setStadiumLocation(String StadiumLocation) {
         this.location = StadiumLocation;
     }
+
     public String getDate() {
-        return Date ;
+        return Date;
     }
 
     public void setDate(String Date) {
         this.Date = Date;
     }
+
     public int getStadiumCapacity() {
         return capacity;
     }
 
     public void setStadiumCapacity(int capacity) {
-        this.capacity=capacity;
+        this.capacity = capacity;
     }
 
+}
 
-
-    public boolean Check_Available(String date ) {
+ /*   public boolean Check_Available(String date ) {
        if (Date.equals(date)) {
            return false ;
        }
@@ -56,7 +59,7 @@ public class Stadium {
    }
 
 
-   }
+   }*/
 
 
 

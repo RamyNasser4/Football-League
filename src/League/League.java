b894ecb4ccfe69eaa4901300a25ac5fc1b84d16b
RamyNasser4 <ramy_rnz@hotmail.com>
2023-11-29 15:34:05 +0200

    initial TopScorers fn

--- src/League/League.java
+++ src/League/League.java
@@ -1,18 +1,38 @@
 package League;
 
 import League.Match.Match;
+import League.Person.Player.Player;
 import League.Team.Team;
 
-public interface League {
+public class League {
 
         Match[] matches=new Match[34];
         Team[] teams=new Team[18];
 
-        void DisplayTopScorers();
-        void DisplayTopGoalKeepers();
+        /*public Player[] DisplayTopScorers(){
+                Player[] topScorers = new Player[3];
+                int currentTopScorer = 0;
+                for (int i = 0; i < 3; i++) {
+                        int maxGoals = -1;
+                        for (int j = 0; j < teams.length; j++) {
+                                for (int k = 0; k < teams[j].getTotal(); k++) {
+                                        if(teams[j].getPlayers()[k].getGoalsScored()>maxGoals &&
+                                                teams[j].getPlayers()[k] != topScorers[0] &&
+                                                teams[j].getPlayers()[k] != topScorers[1] )
+                                        {
+                                                maxGoals = teams[j].getPlayers()[k].getGoalsScored();
+                                                topScorers[currentTopScorer] = teams[j].getPlayers()[k];
+                                                ++currentTopScorer;
+                                        }
+                                }
+                        }
+                }
+                return topScorers;
+        }*/
+        /*void DisplayTopGoalKeepers();
         void DisplayByGoals();
         void DisplayByAge();
-        void DisplayByDate(String date);
+        void DisplayByDate(String date);*/
 
 
 }

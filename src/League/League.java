9a219697f806d03f1c86a91e3f47868c64e38ea2
abdelrahmn594 <abdelrahmanemad999@gmail.com>
2023-12-01 18:57:48 +0200

    DisplayTeamByGoals fn league class

--- src/League/League.java
+++ src/League/League.java
@@ -4,32 +4,35 @@ import League.Match.Match;
 import League.Person.Player.Goalkeeper;
 import League.Person.Player.Player;
 import League.Team.Team;
+import League.Team.*;
 
-public class League {
+import java.util.Arrays;
 
-        Match[] matches=new Match[34];
-        Team[] teams=new Team[18];
+public interface League {
 
-        /*public Player[] DisplayTopScorers(){
-                Player[] topScorers = new Player[3];
-                int currentTopScorer = 0;
-                for (int i = 0; i < 3; i++) {
-                        int maxGoals = -1;
-                        for (int j = 0; j < teams.length; j++) {
-                                for (int k = 0; k < teams[j].getTotal(); k++) {
-                                        if(teams[j].getPlayers()[k].getGoalsScored()>maxGoals &&
-                                                teams[j].getPlayers()[k] != topScorers[0] &&
-                                                teams[j].getPlayers()[k] != topScorers[1] )
-                                        {
-                                                maxGoals = teams[j].getPlayers()[k].getGoalsScored();
-                                                topScorers[currentTopScorer] = teams[j].getPlayers()[k];
-                                                ++currentTopScorer;
-                                        }
-                                }
-                        }
-                }
-                return topScorers;
-        }*/
+    Match[] matches = new Match[34];
+    Team[] teams = new Team[18];
+
+    /*public Player[] DisplayTopScorers(){
+            Player[] topScorers = new Player[3];
+            int currentTopScorer = 0;
+            for (int i = 0; i < 3; i++) {
+                    int maxGoals = -1;
+                    for (int j = 0; j < teams.length; j++) {
+                            for (int k = 0; k < teams[j].getTotal(); k++) {
+                                    if(teams[j].getPlayers()[k].getGoalsScored()>maxGoals &&
+                                            teams[j].getPlayers()[k] != topScorers[0] &&
+                                            teams[j].getPlayers()[k] != topScorers[1] )
+                                    {
+                                            maxGoals = teams[j].getPlayers()[k].getGoalsScored();
+                                            topScorers[currentTopScorer] = teams[j].getPlayers()[k];
+                                            ++currentTopScorer;
+                                    }
+                            }
+                    }
+            }
+            return topScorers;
+    }*/
       /*  public Player[] DisplayTopGoalKeepers() {
                 Player[] topKeepers = new Player[3];
                 int max = 0;
@@ -49,19 +52,54 @@ public class League {
                         }
                 }
                 return topKeepers;
-        }
-        /*void DisplayByGoals();
-        void DisplayByAge();
-        void DisplayByDate(String date);
-        default void DisplayTeamByAvgAge(){
-                double min=0;
-                Team[] teamscopy= Arrays.copyOf(teams,teams.length);
-                for(Team teams:teamscopy){
-                        Arrays.sort(teamscopy);
-                }
-                for(Team teams:teamscopy){
-                        System.out.println(teams.getName()+" Average Age is "+teams.AvgTeamAge());
-                }*/
+        }*/
+    void DisplayByGoals();
 
+    void DisplayByAge();
+
+    void DisplayByDate(String date);
+
+    default void DisplayTeamByAvgAge() {
+        double min = 0;
+        Team[] teamscopy = Arrays.copyOf(teams, teams.length);
+        for (Team teams : teamscopy) {
+            Arrays.sort(teamscopy);
+        }
+        for (Team teams : teamscopy) {
+            System.out.println(teams.getName() + " Average Age is " + teams.AvgTeamAge());
+        }
+    }
+
+    default void DisplayTeamsByGoals() {
+        int[] teamGoals = new int[teams.length];
+        int totalTeamGoals = -1;
+        for (int i = 0; i < teams.length; i++) {
+            for (int j = 0; j < teams[i].getTotal(); j++) {
+                teamGoals[i] += teams[i].getPlayers()[j].getGoalsScored();
+            }
+            if (teamGoals[i] > totalTeamGoals) {
+                totalTeamGoals = teamGoals[i];
+            }
+
+        }
+        // option 1
+        /*Arrays.sort(teams, Comparator.comparingInt((Team team) -> teamGoals[Arrays.asList(teams).indexOf(team)]).reversed());*/
+        // option 2
+        for (int i = 0; i < teams.length; i++) {
+            for (int j = i; j < teams.length; j++) {
+                if (teamGoals[i] < teamGoals[j]) {
+                    Team temp = teams[i];
+                    teams[i] = teams[j];
+                    teams[j] = temp;
+                    int tempGoals = teamGoals[i];
+                    teamGoals[i] = teamGoals[j];
+                    teamGoals[j] = tempGoals;
+                }
+            }
+        }
+    }
 
 }
+
+
+

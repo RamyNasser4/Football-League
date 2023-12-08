e892cfcaf6c0b80c215c906af5247af57e9b4a84
abdelrahmn594 <abdelrahmanemad999@gmail.com>
2023-12-09 00:08:07 +0200

    MatchInfo icons

--- src/League/GUI/MatchInfo.java
+++ src/League/GUI/MatchInfo.java
@@ -8,47 +8,46 @@ public class MatchInfo extends JPanel {
     public MatchInfo() {
         this.setPreferredSize(new Dimension(980, 720));
         this.setBackground(Color.BLACK);
-        this.setLayout(new GridLayout(3, 1)); // Adjusted to 3 rows, 1 column
+        this.setLayout(new GridLayout(3, 1)); // 3 rows, 1 column
+
+        ImageIcon refereeIcon = new ImageIcon("src/Assets/referee.png");
+        ImageIcon dateIcon = new ImageIcon("src/Assets/clock.png");
+        ImageIcon stadiumIcon = new ImageIcon("src/Assets/stadium.png");
 
         JPanel panel1 = new JPanel(new GridLayout(1, 1));
         JPanel panel2 = new JPanel(new GridLayout(1, 2));
         JPanel panel3 = new JPanel(new GridLayout(1, 1));
 
-        JPanel panelLabel1 = createPanel("Team1 vs Team2", Color.darkGray);
-        JPanel panelLabel2 = createPanel("DATE", Color.gray);
-        JPanel panelLabel3 = createPanel("REFEREE", Color.gray);
-        JPanel panelLabel4 = createPanel("STADIUM", Color.gray);
+        JLabel label1 = new JLabel(": Team1 vs Team2");
+        label1.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 60));
+        label1.setForeground(Color.WHITE);
+        label1.setHorizontalAlignment(JLabel.CENTER);
+        panel1.setBackground(Color.darkGray);
+        panel1.add(label1);
 
-        Font bigFont = new Font(Font.SANS_SERIF, Font.BOLD, 60);
-        JLabel label1 = (JLabel) panelLabel1.getComponent(0);
-        label1.setFont(bigFont);
+        JLabel label2 = new JLabel(": DATE", dateIcon, JLabel.CENTER);
+        label2.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 40));
+        label2.setForeground(Color.WHITE);
+        label2.setIconTextGap(20);
+        panel2.setBackground(Color.gray);
+        panel2.add(label2);
 
-        panel1.add(panelLabel1);
-        panel2.add(panelLabel2);
-        panel2.add(panelLabel3);
-        panel3.add(panelLabel4);
+        JLabel label3 = new JLabel(": REFEREE", refereeIcon, JLabel.CENTER);
+        label3.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 40));
+        label3.setForeground(Color.WHITE);
+        label3.setIconTextGap(20);
+        panel2.add(label3);
+
+        JLabel label4 = new JLabel(": STADIUM", stadiumIcon, JLabel.CENTER);
+        label4.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 40));
+        label4.setForeground(Color.WHITE);
+        label4.setIconTextGap(20);
+        panel3.setBackground(Color.gray);
+        panel3.add(label4);
 
         this.add(panel1);
         this.add(panel2);
         this.add(panel3);
-        this.setVisible(false);
-    }
-
-    // create panel with label
-    private JPanel createPanel(String labelText, Color panelColor) {
-        JPanel panel = new JPanel(new BorderLayout());
-        JLabel label = new JLabel(labelText);
-
-        Font boldFont = new Font(Font.SANS_SERIF, Font.BOLD, 40);
-        Color whiteColor = Color.WHITE;
-
-        label.setFont(boldFont);
-        label.setForeground(whiteColor);
-        label.setHorizontalAlignment(JLabel.CENTER);
-
-        panel.add(label, BorderLayout.CENTER);
-        panel.setBackground(panelColor);
-
-        return panel;
+        this.setVisible(true);
     }
 }
